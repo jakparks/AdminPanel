574a2eb0ff17c2fb3061ddd0bd5feb14efb165e7
jakparks <jakparks@cisco.com>
2015-11-04 10:27:03 -0500

    Updated run.sh to include sample user/password

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@
 export LD_LIBRARY_PATH="/apps/swapps/gcc4/lib64"
 #Set this to your node.js binary folder
 export PATH="$PATH:/apps/node/bin"
+#This is the username to access the panel.  This user should be authorized to see/edit the DB
+export ADMIN_USER="EXAMPLE_USER"
+#Password for above user
+export ADMIN_PASS="EXAMPLE_PASS"
 
 forever start server.js
